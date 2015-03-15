59562498e704ffc393f781b7dfac997c78cdfd94
Bartosz Celmer <cb1986ster@gmail.com>
2015-03-15 13:27:51 +0100

    Upgrade freeram lib

--- lib/freeram/freeram.h
+++ lib/freeram/freeram.h
@@ -1,7 +1,6 @@
-int freeRam () {
-  extern int __heap_start, *__brkval; 
-  int v; 
-  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
-}
+#ifndef _FREERAM_H_
+#define _FREERAM_H_ 1
 
+int freeRam ();
 
+#endif
